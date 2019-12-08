7446caab57a5446134a40ab9dd97b83093d1791d
elizavetaAnd <55841090+elizavetaAnd@users.noreply.github.com>
2019-12-08 21:25:28 +0400

    Update Program.cs

--- Form1/Program.cs
+++ Form1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static Lab4Rassohin.ClipClass;
+using static Andreeva_4.ClipClass;
 namespace Form1
 {
     static class Program
